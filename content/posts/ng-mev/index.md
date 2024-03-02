0daabaa64f2268fe2bd7f95fb4254bb599fcc546
benny <benoitb@tutanota.de>
2024-03-03 10:05:57 +1100

    Add dynamic fee widget

--- content/posts/ng-mev/index.md
+++ content/posts/ng-mev/index.md
@@ -16,7 +16,7 @@ tags:
 New Generation (NG) pools, [introduced in late 2023](https://etherscan.io/tx/0x2c7c9319d9b9cc067c38000e450a9df09fee9ec6c7dde173deec73d37ae0e15d), are a new iteration over the original Stableswap implementation. 
 Based on the same invariant formula, [NG pools](https://curve.fi/#/ethereum/pools?filter=stableng) introduce a number of new features such as dynamic fees, better oracle integration, transferless swaps, the ability to create pools for up to eight tokens of varying types (rebasing, ERC-4626 tokens, oracle-linked tokens).
 
-Among these different enhancements, this article will focus on dynamic fees. While the original Stableswap pools applied a flat fee to all trades, the fee charged by NG pool is a function of its balances and exchange rate. 
+Among these different enhancements, this article will focus on dynamic fees. While the original Stableswap pools applied a flat fee to all trades, the fee charged by NG pool is a function of its balances and internal rates. 
 This allows pools to charge more when liquidity is imbalanced and assets off-peg. 
 This has the advantage of offering better risk-adjusted returns to LPs: when the pool is imbalanced and they hold more of the risky, depegging asset, they receive a higher compensation through fees. 
 It also improves the stability and security of the pool by making it more expensive to execute trades that significantly imbalance the pool. 
@@ -32,4 +32,74 @@ For example, if a large buy order for Asset A is detected by an attacker, they m
 </div>
 
 
-Dynamic fees counteract this strategy by increasing the cost of executing trades that significantly skew the pool's balance.
+Dynamic fees counteract this strategy by increasing the cost of executing trades that significantly skew the pool's balance. After a brief overview of how dynamic fees work on NG pools, we will look at MEV activity to try and assess their actual effect.
+
+## Dynamic Fees
+
+The formula used to calculate the dynamic fees uses the pool's base fee and an `offpeg_fee_multiplier` variable, whose value determines by how much the base fee will increase in proportion to liquidity imbalances. The full logic for the calculation can be found in the [CurveStableSwapNG contract](https://github.com/curvefi/stableswap-ng/blob/ec972b331da21d919f78943e00bf9398970eca54/contracts/main/CurveStableSwapNG.vy#L887-L900):
+
+```python
+def _dynamic_fee(xpi: uint256, xpj: uint256, _fee: uint256) -> uint256:
+
+    _offpeg_fee_multiplier: uint256 = self.offpeg_fee_multiplier
+    if _offpeg_fee_multiplier <= FEE_DENOMINATOR:
+        return _fee
+
+    xps2: uint256 = (xpi + xpj) ** 2
+    return unsafe_div(
+        unsafe_mul(_offpeg_fee_multiplier, _fee),
+        unsafe_add(
+            unsafe_sub(_offpeg_fee_multiplier, FEE_DENOMINATOR) * 4 * xpi * xpj / xps2,
+            FEE_DENOMINATOR
+        )
+    )
+```
+
+or, in a slightly more readable mathematical notation: $\frac{fee * offpeg\\_fee\\_multiplier}{\frac{(offpeg\\_fee\\_multiplier - 10^{10}) * 4 * xp_i * xp_j}{(xp_i + xp_j)^2} + 10^{10}}$
+
+where `fee` is the pool's base fee, `offpeg_fee_multiplier` is the multiplier and `FEE_DENOMINATOR` is a constant equal to $10^{10}$ used to handle the fees' precision. The variables ${xp_i}$ and ${xp_j}$ are the balances of the tokens of the pool adjusted for decimals and the pool's internal rates. 
+For instance, if one of the pool's tokens uses rates from an external oracle like wstETH or is an ERC-4626 vault token with a share to asset ratio, these rates will be applied to the pools' balances before computing the dynamic fee. 
+
+
+<script src="../../js/ng-mev/dynamicFee.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<style>
+    .chart-container {
+        width: 45%;
+        display: inline-block;
+    }
+    .widget-container {
+        text-align: left;
+    }
+    input[type="range"], input[type="number"] {
+        width: 35%;
+        margin: 5px;
+    }
+</style>
+
+
+<div class="widget-container">
+<label for="xpiSlider">xpi:</label>
+<input type="range" id="xpiSlider" min="1" max="100" value="50">
+<input type="number" id="xpiNumber" min="1" max="100" value="50">
+<br>
+<label for="xpjSlider">xpj:</label>
+<input type="range" id="xpjSlider" min="1" max="100" value="50">
+<input type="number" id="xpjNumber" min="1" max="100" value="50">
+<br>
+<label for="feeSlider">Fee:</label>
+<input type="range" id="feeSlider" min="0" max="10000000" step="100000" value="4000000">
+<input type="number" id="feeNumber" min="0" max="10000000" step="100000" value="4000000">
+<br>
+<label for="offpegSlider">Offpeg Multiplier:</label>
+<input type="range" id="offpegSlider" min="0" max="100000000000" step="1000000000" value="20000000000">
+<input type="number" id="offpegNumber" min="0" max="100000000000" step="1000000000" value="20000000000"><br>
+</div>
+
+<div class="chart-container">
+    <canvas id="barChart" height="250px"></canvas>
+</div>
+
+<div class="chart-container">
+    <canvas id="lineChart" height="280px"></canvas>
+</div>
---
+++ static/js/ng-mev/dynamicFee.js
@@ -0,0 +1,164 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const xpiSlider = document.getElementById('xpiSlider');
+    const xpjSlider = document.getElementById('xpjSlider');
+    const feeSlider = document.getElementById('feeSlider');
+    const offpegSlider = document.getElementById('offpegSlider');
+    const xpiNumber = document.getElementById('xpiNumber');
+    const xpjNumber = document.getElementById('xpjNumber');
+    const feeNumber = document.getElementById('feeNumber');
+    const offpegNumber = document.getElementById('offpegNumber');
+
+    const updateValues = () => {
+        xpiNumber.value = xpiSlider.value;
+        xpjNumber.value = xpjSlider.value;
+        feeNumber.value = feeSlider.value;
+        offpegNumber.value = offpegSlider.value;
+        updateBarChart();
+        updateLineChart();
+    };
+
+    xpiSlider.oninput = () => updateValues();
+    xpjSlider.oninput = () => updateValues();
+    feeSlider.oninput = () => updateValues();
+    offpegSlider.oninput = () => updateValues();
+    xpiNumber.oninput = () => updateValues();
+    xpjNumber.oninput = () => updateValues();
+    feeNumber.oninput = () => updateValues();
+    offpegNumber.oninput = () => updateValues();
+
+    let barChart, lineChart;
+
+    const dynamicFee = (xpi, xpj, fee, offpeg) => {
+        const FEE_DENOMINATOR = 1e10;
+        if (offpeg <= FEE_DENOMINATOR) return fee;
+        const xps2 = (Number(xpi) + Number(xpj)) ** 2;
+        return (offpeg * fee) / ((offpeg - FEE_DENOMINATOR) * 4 * xpi * xpj / xps2 + FEE_DENOMINATOR);
+    };
+
+    const updateBarChart = () => {
+        const xpi = xpiSlider.value;
+        const xpj = xpjSlider.value;
+        if (barChart) {
+            barChart.data.datasets[0].data = [xpi, xpj];
+            barChart.update();
+        } else {
+            const ctx = document.getElementById('barChart').getContext('2d');
+            barChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: ['xpi', 'xpj'],
+                    datasets: [{
+                        label: 'Pool Balances',
+                        data: [xpi, xpj],
+                        backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)'],
+                        borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)'],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    },
+                    plugins: {
+                        legend: {
+                            display: false
+                        },
+                        title: {
+                            display: true,
+                            text: 'Pool Balances',
+                            font: {
+                                size: 14
+                            }
+                        }
+                    }
+                }
+            });
+        }
+    };
+
+    const updateLineChart = () => {
+        let labels = [], data = [], userValue = [];
+        for (let i = 1; i <= 99; i++) {
+            labels.push(`${i}:${100 - i}`);
+            data.push(dynamicFee(i , (100 - i), feeSlider.value, offpegSlider.value));
+        }
+        const xpSum = Number(xpiSlider.value) + Number(xpjSlider.value)
+        console.log(xpSum)
+        const userXpi = Math.round((xpiSlider.value / xpSum) * 100)
+        const userXpj = Math.round((xpjSlider.value / xpSum) * 100)
+        const userDynamicFee = dynamicFee(userXpi, userXpj, feeSlider.value, offpegSlider.value);
+        userValue.push({
+            x: `${userXpi}:${userXpj}`,
+            y: userDynamicFee
+        });
+        console.log(userValue)
+
+        if (lineChart) {
+            lineChart.data.labels = labels;
+            lineChart.data.datasets[0].data = data;
+            lineChart.data.datasets[1].data = userValue;
+            lineChart.update();
+        } else {
+            const ctx = document.getElementById('lineChart').getContext('2d');
+            lineChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        label: 'Dynamic Fee vs Pool Balances',
+                        data: data,
+                        fill: false,
+                        pointRadius: 0,
+                        borderColor: 'rgb(75, 192, 192)',
+                        tension: 0.1,
+                        order: 2
+                    }, {
+                        label: 'User Selected Value',
+                        data: userValue,
+                        fill: false,
+                        pointRadius: 5,
+                        pointBackgroundColor: 'red',
+                        borderColor: 'rgb(75, 192, 192)',
+                        tension: 0.1,
+                        showLine: false,
+                        order: 1
+                    }]
+                },
+                options: {
+                    maintainAspectRatio: false,
+
+                    plugins: {
+                        legend: {
+                            display: false
+                        },
+                        title: {
+                            display: true,
+                            text: 'Dynamic Fee vs Ratio xpi/xpj',
+                            font: {
+                                size: 14
+                            }
+                        }
+                    },
+                    scales: {
+                        x: {
+                            title: {
+                                display: true,
+                                text: 'xpi:xpj proportions (%)'
+                            }
+                        },
+                        y: {
+                            title: {
+                                display: true,
+                                text: 'Dynamic fee'
+                            }
+                        }
+                    }
+                }
+            });
+        }
+    };
+
+    updateValues();
+});
